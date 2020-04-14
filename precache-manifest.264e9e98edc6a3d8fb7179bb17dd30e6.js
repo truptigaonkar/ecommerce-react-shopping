ba83a3bc8aa4359390ff240b2ebe29a8bb4b1538
truptigaonkar <trups11.trupti@gmail.com>
2020-04-14 13:33:30 +0200

    Updates

---
+++ precache-manifest.264e9e98edc6a3d8fb7179bb17dd30e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7162fde737f5da8231cc9b45c76c3e1",
+    "url": "/ecommerce-react-shopping/index.html"
+  },
+  {
+    "revision": "26952e655b6724a49348",
+    "url": "/ecommerce-react-shopping/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "4cb3f59dd536d3e820a3",
+    "url": "/ecommerce-react-shopping/static/js/2.3b797ff9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ecommerce-react-shopping/static/js/2.3b797ff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26952e655b6724a49348",
+    "url": "/ecommerce-react-shopping/static/js/main.91fd088f.chunk.js"
+  },
+  {
+    "revision": "a502f252617812375280",
+    "url": "/ecommerce-react-shopping/static/js/runtime-main.b0eb60a6.js"
+  }
+]);
